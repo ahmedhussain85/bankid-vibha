06708fb11e35d582f66d5238701ecb4c8003edac
Vibha Thumar <savaliya.vibha@gmail.com>
2022-10-17 21:54:09 +0200

    app.js changes

--- app.js
+++ app.js
@@ -13,34 +13,11 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
   })
 
-  const postmanRequest = require('postman-request')
-  
-app.get('/forcast' , (req, res) => {
-  const latitude = 20.5937
-  const longitude = 78.9629
-  const url ='http://api.weatherstack.com/current?access_key=a94de50a3cfc9421255fc77a94b0c600&query='+latitude+','+longitude+'&units=f'
-
-    postmanRequest({ url : url, json: true}, (error, {body} = {}) => {
-    //https.request({ url, json: true}, (error, {body} = {}) => {
-    
-        if(error){
-            //callback('Unable to connect weather service!', undefined)
-            console.log('Unable to connect weather service!')
-        }else if(body.error){
-            console.log('Unable to find location. Try another search.')
-        }else{
-          
-            res.send(body.current.weather_descriptions[0] + ', It is currently '+body.current.temperature +' degree out. It feels like '+body.current.feelslike+' degree out')
-        }
-        
-    })
-})
-
 app.get('/auth/:personalNumber', async (req, res) => {
     const data = JSON.stringify({
-       // personalNumber  : req.params.personalNumber, 
+        personalNumber  : req.params.personalNumber, 
         endUserIp       : userIp,
-        //requirement     : {"allowFingerprint": true},
+        requirement     : {"allowFingerprint": true},
         })
         
         
@@ -59,9 +36,7 @@ app.get('/auth/:personalNumber', async (req, res) => {
             passphrase: 'qwerty123',
             rejectUnauthorized: false,
             resolveWithFullResponse: true,
-            minVersion: "TLSv1.2",
-            maxVersion: "TLSv1.2"
-            //timeout: 5000,
+            timeout: 5000,
           }
           
       
