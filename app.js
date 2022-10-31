89bb996d90cbc18c16589c5af5c9e7a350d0c308
Vibha Thumar <savaliya.vibha@gmail.com>
2022-10-31 17:21:52 +0100

    Auth without personal number

--- app.js
+++ app.js
@@ -14,15 +14,15 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
   })
 
-app.get('/auth/:personalNumber', async (req, res) => {
+app.get('/auth', async (req, res) => {
   const data = JSON.stringify({
-    personalNumber  : req.params.personalNumber, 
+    //personalNumber  : req.params.personalNumber, 
     endUserIp       : userIp,
     requirement     : {"allowFingerprint": true},
   })
   
   const options = {
-    hostname: 'demo.bankid.com',
+    hostname: 'appapi2.test.bankid.com',
     port: 443,
     path: '/rp/v5.1/auth',
     method: 'POST',
@@ -30,27 +30,60 @@ app.get('/auth/:personalNumber', async (req, res) => {
       'Content-Type': 'application/json',
       'Content-Length': data.length
     },
-    json: true,
-    key: fs.readFileSync('./bankid-test.key.pem'),
-    cert: fs.readFileSync('./bankid-test.crt.pem'),  
+    json: true, 
+    pfx: fs.readFileSync('./FPTestcert4_20220818.p12'),
     passphrase: 'qwerty123',
     rejectUnauthorized: false,
     resolveWithFullResponse: true,
     timeout: 5000,
   }
-  https.request(options, (res) => {
-    console.log("statusCode:" +res.statusCode)
-    //console.log(res)
-    res.on('data', d => {
-      process.stdout.write(d)
-    })
-  }).write(data)
+ 
+  let d = await doPostToDoItem(data, options);
+        console.log(d);
+        parsedData = JSON.parse(d);
 
-  res.send()
+        this.qrStartSecret = parsedData.qrStartSecret;
+        this.orderRef = parsedData.orderRef;
+        this.qrStartToken = parsedData.qrStartToken;
+
+  res.send('orderRef : ' +parsedData.orderRef)
           
 })
+
+
+
+
 
 app.listen(port, () => {
     console.log('Server is up on port: ' + port)
 })
+
+const doPostToDoItem = async (data, options) => {
+  //console.log('dopost function')
+  let responseBody = '';
+
+  let p = new Promise((resolve, reject) => {
+      const req = https.request(options, (res) => {
+          //res.setEncoding('utf8');
+
+          res.on('data', (chunk) => {
+              responseBody += chunk;
+          });
+
+          res.on('end', () => {
+              resolve(JSON.parse(responseBody));
+          });
+      });
+
+      req.on('error', (err) => {
+          reject(err);
+      });
+
+      req.write(data)
+      req.end();
+  });
+
+  await p;
+  return responseBody;
+}
 
